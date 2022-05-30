ec5c89f3db1cb6a5516f957c30f783796645f812
hikari <tanimoto0709@gmail.com>
2022-05-31 05:36:29 +0900

    add: 開発環境の構築

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    'js/app': ['./src/App.jsx'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: ['babel-loader'],
+        exclude: ['/node_modules/'],
+      },
+    ],
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+  ]
+}
