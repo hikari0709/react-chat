ff80c640becb41ff7e66282b527007566363e8c0
hikari <tanimoto0709@gmail.com>
2022-06-04 19:42:27 +0900

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  return <h1>Hello World!</h1>;
+  return <h1>Hello React!!</h1>;
 };
 ReactDOM.render(<App />, document.getElementById('app'));
