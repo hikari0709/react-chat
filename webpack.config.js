d04af379196c32561edecc9dcf23d74b651795ef
hikari <tanimoto0709@gmail.com>
2022-06-04 09:26:51 +0900

    update:main 開発環境の構築

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     'js/app': ['./src/App.jsx'],
   },
   output: {
-    path: path.resolve(__dirname, 'dist/'),
+    path: path.resolve(__dirname, 'public/'),
     publicPath: '/',
   },
   module: {
